cb8d854f84ad180bcf19fcc1200a0fac8c4ce96e
Akbar Haleel <akbarhaleel508@gmail.com>
2024-05-25 14:31:33 +0530

    add routes and create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
 
 export default function App() {
   return (
     <div>
-      <h1 className='text-red-600'>Akbar haleel</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
